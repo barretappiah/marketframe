d74e57b9b620245652e019323ccaf2cbd5993de2
Barret108 <barretappiah@gmail.com>
2025-12-22 19:49:32 -0700

    Moving averages

--- app.py
+++ app.py
@@ -1,23 +1,54 @@
 import streamlit as st
 from data import df_maker
 import plotly.express as px
+import plotly.graph_objects as go
 
 # BASIC SETTINGS
 st.title("Tesla, Inc. (TSLA)")
 ticker = 'TSLA'
-duration = 20
+duration = 100
 interval = '1h'
+
 
 # BUILD DATA-FRAME
 def get_info(ticker, duration, interval):
     df = df_maker(ticker, duration, interval)
     return df
 
+
 # PLOT GRAPH
 def interactive_plot(df):
-    plot = px.line(df, x=df.index, y='Close', line_shape='linear')
-    st.plotly_chart(plot)
+    fig = go.Figure()
 
+    # Close Price
+    fig.add_trace(go.Scatter(
+        x=df.index,
+        y=df['Close'],
+        mode='lines',
+        name='Close'
+    ))
+
+    # MA 20
+    fig.add_trace(go.Scatter(
+        x=df.index,
+        y=df['MA_20'],
+        mode='lines',
+        name='MA 20'
+    ))
+
+        # MA 50
+    fig.add_trace(go.Scatter(
+        x=df.index,
+        y=df['MA_50'],
+        mode='lines',
+        name='MA 50'
+    ))
+
+    fig.update_layout(
+        legend_title = "Indicators"
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
 
 def main():
     # Dataframe
--- data.py
+++ data.py
@@ -2,7 +2,14 @@ import yfinance as yf
 from datetime import date, datetime, timedelta
 
 def get_data(ticker, begin, stop, gap):
+    # Close Prices
     df = yf.download(ticker, start=begin, end=stop, interval=gap, auto_adjust=True)
+
+    # Moving Averages
+    df["MA_20"] = df['Close'].rolling(20).mean()
+    df["MA_50"] = df['Close'].rolling(50).mean()
+
+    # Remove Useless Space
     df.dropna(inplace=True)
     df.columns = df.columns.droplevel(1)
     return df
