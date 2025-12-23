67cd2fb84e7138c4f60f14ad3100a6d8b7feab98
Barret108 <barretappiah@gmail.com>
2025-12-22 19:05:53 -0700

    Better compatibility
    More accessible settings change and ability to use df more than once without re gathering data

--- app.py
+++ app.py
@@ -2,14 +2,28 @@ import streamlit as st
 from data import df_maker
 import plotly.express as px
 
-st.title("Title")
+# BASIC SETTINGS
+st.title("Tesla, Inc. (TSLA)")
+ticker = 'TSLA'
+duration = 20
+interval = '1h'
 
-df = (df_maker('TSLA', 30))
+# BUILD DATA-FRAME
+def get_info(ticker, duration, interval):
+    df = df_maker(ticker, duration, interval)
+    return df
 
+# PLOT GRAPH
 def interactive_plot(df):
-    if df is None or df.empty:
-        raise ValueError('No Data')
     plot = px.line(df, x=df.index, y='Close', line_shape='linear')
     st.plotly_chart(plot)
 
-interactive_plot(df)
+
+def main():
+    # Dataframe
+    df = get_info(ticker, duration, interval)
+
+    # Graph
+    interactive_plot(df)
+
+main()
--- data.py
+++ data.py
@@ -1,17 +1,16 @@
 import yfinance as yf
 from datetime import date, datetime, timedelta
 
-def get_data(ticker, begin, stop):
-    df = yf.download(ticker, start=begin, end=stop, auto_adjust=True)
+def get_data(ticker, begin, stop, gap):
+    df = yf.download(ticker, start=begin, end=stop, interval=gap, auto_adjust=True)
     df.dropna(inplace=True)
     df.columns = df.columns.droplevel(1)
     return df
 
-def df_maker(ticker, duration):
+def df_maker(ticker, duration, interval):
 
     begin = date.today() - timedelta(duration)
     stop = date.today()
 
-    df = get_data(ticker, begin, stop)
+    df = get_data(ticker, begin, stop, interval)
     return df
-
